b165ccde05aeafdedcd14c6acca78b6776c4cc80
xdavidwu <xdavidwuph@gmail.com>
2020-02-19 22:40:15 +0800

    update discord.js, read token from file

--- discord-voice-channel-bot.js
+++ discord-voice-channel-bot.js
@@ -1,12 +1,19 @@
-const Discord=require('discord.js');
-const client=new Discord.Client();
-client.on('voiceStateUpdate',(oldMember,newMember)=>{
-	const channel=oldMember.guild.channels.find(ch=>ch.name==='voice-log');
-	if(!channel)return;
-	if(oldMember.voiceChannel===undefined&&newMember.voiceChannel!==undefined){
-		channel.send(`${newMember} joins ${newMember.voiceChannel}`);
-	}else if(newMember.voiceChannel===undefined){
-		channel.send(`${oldMember} leaves ${oldMember.voiceChannel}`);
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+	console.log('ready');
+});
+client.on('voiceStateUpdate', (oldState, newState) => {
+	const channel = oldState.guild.channels.cache.find(ch => ch.name === 'voice-log');
+	if (!channel) {
+		console.log('#voice-log not found');
+		return;
+	}
+	if (!oldState.channelID && newState.channelID) {
+		channel.send(`${newState.member} joins ${newState.channel}`);
+	} else if (oldState.channelID && !newState.channelID) {
+		channel.send(`${oldState.member} leaves ${oldState.channel}`);
 	}
 });
-client.login('token');
+client.login(fs.readFileSync('token', 'utf8').trim()).catch(console.error);
